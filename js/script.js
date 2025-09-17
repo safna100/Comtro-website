13dba6c48416676d96a9b4e5e2e6156a69f2a2ad
safna100 <khadeejasafna857@gmail.com>
2025-09-17 12:18:30 +0530

    coding

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.toggle-bt').click(function(){
+        $('.toggle-bt').toggleClass('toggled');
+        $('header nav ul').toggleClass('toggled');
+    })
+
+})
